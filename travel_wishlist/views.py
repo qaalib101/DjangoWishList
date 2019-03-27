bca196b5bc9f673039d6144b289c37c5b794ebba
Qaalib Farah <[si8822fb@gmail.com]>
2019-03-27 09:25:25 -0500

    added more templates and more functionality

--- travel_wishlist/views.py
+++ travel_wishlist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
 from .forms import NewPlaceForm
 # Create your views here.
@@ -12,9 +12,7 @@ def place_list(request):
         if form.is_valid():
             place.save()
             return redirect('place_list')
-
-
-    places = Place.objects.filter(visited=True).order_by('name')
+    places = Place.objects.filter(visited=False).order_by('name')
     new_place_form = NewPlaceForm()
     return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
 
@@ -22,3 +20,12 @@ def place_list(request):
 def places_visited(request):
     visited = Place.objects.filter(visited=True)
     return render(request, 'travel_wishlist/visited.html', {'visited': visited})
+
+def place_was_visited(request):
+    if request.method == 'POST':
+        pk = request.POST.get('pk')
+        place = get_object_or_404(Place, pk=pk)
+        place.visited = True
+        place.save()
+
+    return redirect('place_list')
